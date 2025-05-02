3631eca9d73dadc642dde5c55209009df4c1b57b
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-05-02 13:46:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ client1.on_message = on_message
 
 
 st.title("INTERFACES MULTIMODALES")
-st.subheader("CONTROL POR VOZ")
+st.subheader("Esta app usa el comando de voz para el control de un sistema")
 
 image = Image.open('comandovoz.jpg')
 
